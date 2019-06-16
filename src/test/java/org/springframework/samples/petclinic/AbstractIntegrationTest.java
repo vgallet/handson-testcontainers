af785c501cf51009292007a0958f7f1b259743e0
Antoine <antoinroux@hotmail.fr>
2019-06-16 20:29:40 +0200

    Add test for screenshot test container virtual browser

--- src/test/java/org/springframework/samples/petclinic/AbstractIntegrationTest.java
+++ src/test/java/org/springframework/samples/petclinic/AbstractIntegrationTest.java
@@ -4,12 +4,23 @@ import org.junit.AfterClass;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AbstractIntegrationTest {
 
     protected static BrowserWebDriverContainer genericContainer;
 
     static {
-        genericContainer = new BrowserWebDriverContainer().withCapabilities(new FirefoxOptions());
+        Map<String, String> envs = new HashMap<>();
+        envs.put("SCREEN_WIDTH", "1366");
+        envs.put("SCREEN_HEIGHT", "768");
+        envs.put("SCREEN_DEPTH", "24");
+
+        genericContainer = (BrowserWebDriverContainer) new BrowserWebDriverContainer()
+            .withCapabilities(new FirefoxOptions())
+            .withEnv(envs);
+
         genericContainer.start();
     }
 
--- src/test/java/org/springframework/samples/petclinic/selenium/OwnersPageIHMTest.java
+++ src/test/java/org/springframework/samples/petclinic/selenium/OwnersPageIHMTest.java
@@ -3,11 +3,17 @@ package org.springframework.samples.petclinic.selenium;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.samples.petclinic.AbstractIntegrationTest;
 import org.springframework.samples.petclinic.UtilsTest;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,5 +41,21 @@ public class OwnersPageIHMTest extends AbstractIntegrationTest {
         // On attends que la page soit correctement chargée
         Thread.sleep(1000);
         assertThat(webDriver.getPageSource()).contains("Jeff Black");
+    }
+
+    @Test
+    public void take_screenshot_jeff_black_owner() throws InterruptedException, IOException {
+        webDriver.get("http://" + dockerIpv4 + ":8080/");
+
+        webDriver.findElement(By.cssSelector("[title*='find owners']")).click();
+        WebElement lastname = webDriver.findElement(By.id("lastName"));
+        lastname.sendKeys("black");
+        lastname.submit();
+        // On attends que la page soit correctement chargée
+        Thread.sleep(1000);
+
+        File outputFile = ((RemoteWebDriver)webDriver).getScreenshotAs(OutputType.FILE);
+        File copied = new File("./screenshot.png");
+        Files.copy(outputFile.toPath(), copied.toPath(), StandardCopyOption.REPLACE_EXISTING);
     }
 }
