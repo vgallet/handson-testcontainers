fee8e6b6effe54d1deb25f235051cf884bdce98a
Victor Gallet <victor.gallet@protonmail.ch>
2019-09-03 12:23:15 +0200

    Correctifs
     * ajout d'un port figé dans le fichier properties
     * suppression des imports inutiles

--- src/test/java/org/springframework/samples/petclinic/AbstractRepositoryTests.java
+++ src/test/java/org/springframework/samples/petclinic/AbstractRepositoryTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.repository.Repository;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
